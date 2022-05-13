1febe9130e5260fbe7c19ce40f5dec5a5425eb6b
garbagemza <garbagemza@gmail.com>
2022-05-13 17:55:14 -0300

    parameterizing container port upon container up

--- download.sh
+++ download.sh
@@ -3,7 +3,9 @@
 do_work() {
     echo "[AZ] downloading scripts"
     echo "[AZ] parameters:"
-    echo "[AZ] container_name: $1"
+    echo "[AZ] container_name:  $1"
+    echo "[AZ] repository_name: $2"
+    echo "[AZ] container_port:  $3"
 
     curl -s https://raw.githubusercontent.com/garbagemza/argos-test/main/down-container.sh -o down-container.sh
     curl -s https://raw.githubusercontent.com/garbagemza/argos-test/main/prune-image.sh -o prune-image.sh
@@ -41,11 +43,16 @@ check_params() {
         echo "expected parameter 'repository_name'."
         exit 1
     fi
+    if [ -z "$3" ]; then
+        echo "expected parameter 'container_port'."
+        exit 1
+    fi
 }
 
 # $1 container_name
 # $2 repository_name
-check_params $1 $2
+# $3 container_port
+check_params $1 $2 $3
 
 # left intentionally at the bottom
-do_work $1 $2
+do_work $1 $2 $3
--- up-container.sh
+++ up-container.sh
@@ -5,15 +5,17 @@ do_run() {
     echo
     echo "This is the up pipeline script."
     echo "this will up $1 container."
+    echo "this will expose $2 port."
     echo
-    up_container $1
+    up_container $1 $2
 }
 
 up_container() {
     echo "[FN] getting up docker container"
-    docker run --name $1 --rm -p 80:8080 -d -v /argos/data:/app/data --env PORT=8080 --env WORKDIR=/app/data argos-cache
+    docker run --name $1 --rm -p $2:8080 -d -v /argos/data:/app/data --env PORT=8080 --env WORKDIR=/app/data $1
 }
 
 # $1 container_name
+# $2 container_port
 # main function is called at the end to ensure the whole file is downloaded.
-do_run $1
+do_run $1 $2
