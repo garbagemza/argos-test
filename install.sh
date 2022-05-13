3fe20582e49717234e2ffd809225b7271b043725
garbagemza <garbagemza@gmail.com>
2022-05-12 21:13:07 -0300

    adding commands to automate pipeline installation

--- install.sh
+++ install.sh
@@ -1,4 +1,40 @@
 #!/bin/bash
+is_linux() {
+    case "$uname -s" in
+    *linux* ) true ;;
+    *Linux* ) true ;;
+    * ) false;;
+    esac
+}
 
-echo "hello, world!"
+do_install() {
+    echo "executing argos pipeline install script"
+    make_directories
+    download_pipeline
+    build_images
+}
 
+make_directories() {
+    echo "making working directories"
+    mkdir -p argos/data
+    mkdir -p argos/src
+}
+
+download_pipeline() {
+    echo "downloading pipeline"
+    git clone --depth 1 https://github.com/garbagemza/argos-repository-cache-job.git argos/src/argos-repository-cache-job
+}
+
+build_images() {
+    echo "building images..."
+    echo "building cache..."
+    #docker build -t argos-cache argos/src/argos-repository-cache-job
+    if is_linux; then
+        echo "running linux!"
+    else
+        echo "not running linux"
+    fi
+}
+
+# call everything at last line so avoid problems downloading file.
+do_install
