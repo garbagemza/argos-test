e670d22d91901a13927a06f99923982b584d514c
garbagemza <garbagemza@gmail.com>
2022-05-13 14:11:24 -0300

    improving liveness probe

--- liveness-probe.sh
+++ liveness-probe.sh
@@ -10,8 +10,22 @@ do_run() {
 
 liveness_probe() {
     echo "[FN] executing liveness probe for containers"
+    echo
+    echo "checking argos-cache $(expect_health 80)"
+    echo
+}
 
-    echo "argos-cache $(curl -s http://localhost/health)"
+check_health() {
+    echo "$(curl -s http://localhost:$1/health)"
+}
+
+expect_health() {
+    local result="$(check_health $1)"
+    if [[ $result == "OK" ]]; then
+        echo "passed"
+    else
+        echo "not passed"
+    fi
 }
 
 # main function is called at the end to ensure the whole file is downloaded.
