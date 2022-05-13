e80df5bcd18292924d75286f40cf1feedf70b507
garbagemza <garbagemza@gmail.com>
2022-05-13 09:48:10 -0300

    improved logging

--- install.sh
+++ install.sh
@@ -16,30 +16,31 @@ is_armv7() {
 
 
 do_install() {
-    echo "executing argos pipeline install script"
+    echo "[FN] executing argos pipeline install script"
     echo
-    echo "platform detected:     $(uname -s)"
-    echo "architecture detected: $(uname -m)"
+    echo "platform:      $(uname -s)"
+    echo "architecture:  $(uname -m)"
     echo
 
     make_directories
     download_pipeline
     build_images
+    clean_directories
 }
 
 make_directories() {
-    echo "making working directories"
+    echo "[FN] making working directories"
     mkdir -p argos/data
     mkdir -p argos/src
 }
 
 download_pipeline() {
-    echo "downloading pipeline"
+    echo "[FN] downloading pipeline"
     git clone --depth 1 https://github.com/garbagemza/argos-repository-cache-job.git argos/src/argos-repository-cache-job
 }
 
 build_images() {
-    echo "building images..."
+    echo "[FN] building images..."
     if is_linux && is_armv7; then
         build_raspberry_images
     else
@@ -49,8 +50,13 @@ build_images() {
 }
 
 build_raspberry_images() {
-    echo "building raspberry pi images"
+    echo "[FN] building raspberry pi images"
     docker build -t argos-cache -f argos/src/argos-repository-cache-job/docker/raspberrypi3.dockerfile argos/src/argos-repository-cache-job
+}
+
+clean_directories() {
+    echo "[FN] cleaning directories"
+    rm -rf argos/src
 }
 # call everything at last line so avoid problems downloading file.
 do_install
