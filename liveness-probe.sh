8685dcd42adca029ddab2d74b09988525d8e750b
garbagemza <garbagemza@gmail.com>
2022-05-13 18:12:32 -0300

    parameterizing liveness probe

--- liveness-probe.sh
+++ liveness-probe.sh
@@ -3,15 +3,15 @@
 do_run() {
     echo
     echo "This is the liveness probe."
-    echo "this will test if all argos services are up."
+    echo "this will test if service is up at port $1."
     echo
-    liveness_probe
+    liveness_probe $1
 }
 
 liveness_probe() {
     echo "[FN] executing liveness probe for containers"
     echo
-    echo "checking argos-cache $(expect_health 80)"
+    echo "checking argos-cache $(expect_health $1)"
     echo
 }
 
@@ -22,11 +22,12 @@ check_health() {
 expect_health() {
     local result="$(check_health $1)"
     if [[ $result == "OK" ]]; then
-        echo "passed"
+        echo "PASSED."
     else
-        echo "not passed"
+        echo "NOT PASSED."
     fi
 }
 
+# $1 container_port
 # main function is called at the end to ensure the whole file is downloaded.
-do_run
+do_run $1
