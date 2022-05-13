ec47b7e07e52c751ad631990e55db18013c8d32b
garbagemza <garbagemza@gmail.com>
2022-05-13 11:19:44 -0300

    improving scrÂipt cleanup

--- download.sh
+++ download.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
 do_work() {
+    echo "[AZ] downloading scripts"
+    echo
+
     curl https://raw.githubusercontent.com/garbagemza/argos-test/main/install.sh -o install.sh
     curl https://raw.githubusercontent.com/garbagemza/argos-test/main/down.sh -o down.sh
     curl https://raw.githubusercontent.com/garbagemza/argos-test/main/up.sh -o up.sh
 
+    echo "[AZ] executing scripts"
+    echo
+
     sh down.sh
     sh install.sh
     sh up.sh
+
+    echo "[AZ] removing scripts"
+    rm down.sh
+    rm install.sh
+    rm up.sh
 }
 
 # left intentionally at the bottom
