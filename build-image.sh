c388c7b37d79beb5871ce3a51662929fa326220b
garbagemza <garbagemza@gmail.com>
2022-05-14 13:47:15 -0300

    extracted build-image script

---
+++ build-image.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+is_linux() {
+        case "$(uname -s)" in
+        *linux* ) true ;;
+        *Linux* ) true ;;
+        * ) false;;
+        esac
+}
+
+is_armv7() {
+        case "$(uname -m)" in
+        *armv7* ) true ;;
+        * ) false ;;
+        esac
+}
+
+
+build_image() {
+    echo "[FN] building image..."
+    if is_linux && is_armv7; then
+        build_raspberrypi_image $1 $2
+    else
+        echo "unknown platform for building image."
+        exit 1
+    fi
+}
+
+build_raspberrypi_image() {
+    echo "[FN] building raspberry pi image"
+    docker build -t $1 -f argos/src/$2/docker/raspberrypi3.dockerfile argos/src/$2
+}
+
+check_params() {
+    if [ -z "$1" ]; then
+        echo "expected parameter 'container_name'."
+        exit 1
+    fi
+    if [ -z "$2" ]; then
+        echo "expected parameter 'repository_name'."
+        exit 1
+    fi
+}
+
+# $1 container_name
+# $2 repository_name
+check_params $1 $2
+
+# left intentionally at the bottom
+build_image $1 $2
