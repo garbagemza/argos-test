14b8dba56080d64a22d3666f5614650666b73ace
garbagemza <garbagemza@gmail.com>
2022-05-14 11:35:06 -0300

    clean up up-container, adding parameter checks.

--- up-container.sh
+++ up-container.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-# TODO: this should be run by docker-compose
-do_run() {
-    echo
-    echo "This is the up pipeline script."
-    echo "this will up $1 container."
-    echo "this will expose $2 port."
-    echo
-    up_container $1 $2
+up_container() {
+    echo "[FN] getting up docker container $1 at port $2"
+    docker run --name $1 --rm -p $2:8080 -d -v /argos/data:/app/data --env PORT=8080 --env WORKDIR=/app/data $1
 }
 
-up_container() {
-    echo "[FN] getting up docker container"
-    docker run --name $1 --rm -p $2:8080 -d -v /argos/data:/app/data --env PORT=8080 --env WORKDIR=/app/data $1
+check_params() {
+    if [ -z "$1" ]; then
+        echo "expected parameter 'container_name'."
+        exit 1
+    fi
+    if [ -z "$2" ]; then
+        echo "expected parameter 'container_port'."
+        exit 1
+    fi
 }
 
 # $1 container_name
 # $2 container_port
+check_params $1 $2
+
+# $1 container_name
+# $2 container_port
 # main function is called at the end to ensure the whole file is downloaded.
-do_run $1 $2
+up_container $1 $2
