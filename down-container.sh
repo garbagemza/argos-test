ec12e21135ff8cfa9ca7fa62c5115474c9b48753
garbagemza <garbagemza@gmail.com>
2022-05-14 11:21:05 -0300

    clean up down container

--- down-container.sh
+++ down-container.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-# TODO: this should be run by docker-compose
-do_run() {
-    echo
-    echo "This is the down-container pipeline script."
-    echo "this will down $1 container."
-    echo
-    down_container $1
-}
-
 down_container() {
-    echo "[FN] getting down docker container"
+    echo "[FN] getting down docker container $1"
     docker container stop $(docker container ls -q --filter name=$1)
 }
 
+check_params() {
+    if [ -z "$1" ]; then
+        echo "expected parameter 'container_name'."
+        exit 1
+    fi
+}
+
+# $1 container_name
+check_params $1
+
 # $1 container name
 # main function is called at the end to ensure the whole file is downloaded.
-do_run $1
+down_container $1
