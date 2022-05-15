68a4a1b8a44ca8f6f38c0eb314007a926e4b8760
garbagemza <garbagemza@gmail.com>
2022-05-15 16:08:46 -0300

    building automatically argos-gateway

--- all.sh
+++ all.sh
@@ -3,19 +3,22 @@
 execute_all() {
     echo "[AZ] *** install everything ***"
 
-    sh down-containers.sh argos-cache argos-repo
-    sh pull-repositories.sh argos-repository-cache-job argos-repository-downloader-job
+    sh down-containers.sh argos-gateway argos-cache argos-repo
+    sh pull-repositories.sh argos-gateway argos-repository-cache-job argos-repository-downloader-job
+    sh build-image.sh argos-gateway argos-gateway
     sh build-image.sh argos-cache argos-repository-cache-job
     sh build-image.sh argos-repo argos-repository-downloader-job
 
     sh clean-repositories.sh
     sh prune-images.sh
 
+    sh up-container.sh argos-gateway 80
     sh up-container.sh argos-cache 8080
     sh up-container.sh argos-repo 8081
 
     sleep 5s
 
+    bash liveness-probe.sh argos-gateway 80
     bash liveness-probe.sh argos-cache 8080
     bash liveness-probe.sh argos-repo 8081
 
